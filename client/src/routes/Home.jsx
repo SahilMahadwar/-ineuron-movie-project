eb33bb694bca0f98c7df7bb3138e2769bf67ed83
SahilMahadwar <accounts@mahadwar.com>
2023-01-30 16:39:21 +0530

    Railway.app bug

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export function Home() {
   return (
     <div>
       <p>Home page</p>
+      <Link to="/admin">Go to admin Admin</Link>
     </div>
   );
 }
