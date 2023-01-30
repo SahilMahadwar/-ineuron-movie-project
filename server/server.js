b44c55c0199c6f8cad9503da3c23d486c884e342
SahilMahadwar <accounts@mahadwar.com>
2023-01-30 15:50:18 +0530

    Express server setup

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+//Load env vars
+dotenv.config({ path: "../.env" });
+
+//Create App
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Api Home
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+// 404 Endpoint
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Ineuron Movie API  running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
