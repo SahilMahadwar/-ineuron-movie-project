5478701c9387e42c48c4c881b1a37a06224410b8
SahilMahadwar <accounts@mahadwar.com>
2023-01-31 23:10:24 +0530

    Changed env dir in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // https://vitejs.dev/config/
+  return defineConfig({
+    plugins: [react()],
+    envDir:
+      process.env.NODE_ENV === "development"
+        ? path.join(__dirname, "../")
+        : path.join(__dirname, "./"),
+  });
+};
