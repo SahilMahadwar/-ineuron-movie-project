381845fb073f0558a1356b70b279ecf8d41b5953
SahilMahadwar <accounts@mahadwar.com>
2023-02-10 23:26:08 +0530

    Bug Solved No Login Button After Logout

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export function Home() {
+  const { getUser } = useAuth();
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div>
       <p>Home page</p>
