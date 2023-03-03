ac995eade657f476c8c03ef027bc77e0d568d646
SahilMahadwar <accounts@mahadwar.com>
2023-03-03 18:54:19 +0530

    Created Protected Route Component

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,5 +1,24 @@
-export function Search() {
-  return <>search</>;
-}
+import { Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import Spinner from "./Spinner";
 
-export default Search;
+export default function ProtectedRoute({ children }) {
+  const { user, isLoading, isError, error } = useAuth();
+
+  if (isLoading) {
+    return (
+      <div className="flex    justify-center items-center">
+        <div className="flex space-x-2 items-center">
+          <Spinner />
+          <p>Accessing User Dashboard...</p>
+        </div>
+      </div>
+    );
+  }
+
+  if (!isLoading && !isError && user) {
+    return <>{children}</>;
+  }
+
+  return <div>{isError && error?.message}</div>;
+}
