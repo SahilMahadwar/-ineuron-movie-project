6ba585833c467d46594a139cb4fc82d21addde2c
SahilMahadwar <accounts@mahadwar.com>
2023-03-07 00:18:27 +0530

    Fixed Active Link Bug

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -83,8 +83,8 @@ export default function ({
                         key={item.name}
                         to={item.href}
                         className={classNames(
-                          item.current
-                            ? "border-indigo-500 text-gray-900"
+                          pathname === item.href
+                            ? "border-brand-500 text-gray-900"
                             : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
                           "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                         )}
