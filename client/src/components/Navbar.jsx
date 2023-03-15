532db2fa4fc6dc6283a4006df6d9bc676458e471
SahilMahadwar <accounts@mahadwar.com>
2023-03-15 16:54:08 +0530

    Active Link Bug

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,8 +15,8 @@ const navigation = [
 const adminNavigation = [
   { name: "Home", href: "/admin", current: true },
   { name: "Movies", href: "/admin/movies", current: false },
-  { name: "Reviews", href: "#", current: false },
-  { name: "Users", href: "#", current: false },
+  { name: "Reviews", href: "/admin/reviews", current: false },
+  { name: "Users", href: "/admin/users", current: false },
 ];
 
 const userNavigation = [
@@ -62,15 +62,15 @@ export default function ({
                   <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
                     {adminNavigation.map((item) => (
                       <Link
-                        to={item.href}
                         key={item.name}
-                        current={item.current ? "page" : undefined}
+                        to={item.href}
                         className={classNames(
-                          item.current
-                            ? "border-indigo-500 text-gray-900"
+                          pathname === item.href
+                            ? "border-brand-500 text-gray-900"
                             : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
                           "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                         )}
+                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </Link>
