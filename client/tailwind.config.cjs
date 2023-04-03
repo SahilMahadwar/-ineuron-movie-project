794f0311580adb34322570b05245d9aea4b9d625
SahilMahadwar <accounts@mahadwar.com>
2023-04-03 23:32:34 +0530

    feat: :sparkles: tailwindcss setup

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,19 @@
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        brand: colors.violet,
+      },
+    },
+    fontFamily: {
+      display: ["InterVariable, -apple-system, system-ui, sans-serif"],
+      body: ["InterVariable, -apple-system, system-ui, sans-serif"],
+    },
+  },
+  plugins: [],
+};
