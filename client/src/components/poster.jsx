618b02b38ca4ba509a754636f01945cdfe2d4b9c
SahilMahadwar <accounts@mahadwar.com>
2023-05-09 00:34:24 +0530

    feat: :sparkles: homepage WIP

---
+++ client/src/components/poster.jsx
@@ -0,0 +1,22 @@
+export const Poster = ({
+  title,
+  posterPath,
+  width = "w-full",
+  height = "h-72",
+}) => {
+  return (
+    <div className="cursor-pointer select-none">
+      <div
+        className={`${width} ${height} bg-gray-300 rounded-lg overflow-hidden`}
+      >
+        <img
+          src={posterPath}
+          alt={title}
+          className="object-cover w-full h-full hover:scale-105 transition duration-500"
+        />
+      </div>
+    </div>
+  );
+};
+
+// w-48 h-72
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,13 +1,86 @@
-import { useUser } from "@/hooks/use-user";
+import { Poster } from "@/components/poster";
+import { axiosApiInstance } from "@/libs/axios-api-Instance";
 import { queryKeys } from "@/libs/react-query/constants";
-import { queryClient } from "@/libs/react-query/query-client";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 export const HomePage = () => {
-  const { user, error, isError, isLoading } = useUser();
+  const { ref, inView, entry } = useInView();
+
+  const fetchMovies = async ({ pageParam = 1 }) => {
+    const { data, status } = await axiosApiInstance.get(`/movies`, {
+      params: {
+        page: pageParam,
+      },
+    });
+
+    return data;
+  };
+
+  const {
+    data: movies,
+    isLoading,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: [queryKeys.movies],
+    queryFn: fetchMovies,
+    getNextPageParam: (lastPage, pages) => {
+      console.log(lastPage);
+      return lastPage.pagination?.next?.page;
+    },
+  });
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h1>Home Page</h1>
+      <div>
+        {movies?.pages?.map.data?.length === 0 ? (
+          <div>no movies found</div>
+        ) : (
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-8 gap-y-12">
+            {movies?.pages?.map((group, i) => (
+              <React.Fragment key={i}>
+                {group.data.map((movie) => (
+                  <Poster
+                    posterPath={movie.poster}
+                    title={movie.name}
+                    key={movie._id}
+                  />
+                ))}
+              </React.Fragment>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="mt-12 flex items-center justify-center">
+        <button
+          ref={ref}
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? "Loading more..."
+            : hasNextPage
+            ? "Load More"
+            : "Nothing more to load"}
+        </button>
+      </div>
+      <div>{isFetching && !isFetchingNextPage ? "ReFetching..." : null}</div>
     </div>
   );
 };
