0817461d42eb0132afb8e6e8c6a7b56ef5da44c2
SahilMahadwar <accounts@mahadwar.com>
2023-06-20 14:35:55 +0530

    feat: :lipstick: add admin layout

---
+++ client/src/layouts/admin-layout.jsx
@@ -0,0 +1,197 @@
+import Logo from "@/assets/logo";
+import { ProfileDropdown } from "@/components/profile-dropdown";
+import Button from "@/components/ui/button";
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  CalendarIcon,
+  FolderIcon,
+  HomeIcon,
+  MenuAlt2Icon,
+  UsersIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import clsx from "clsx";
+
+import { Fragment, useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const navigation = [
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+  { name: "Movies", href: "#", icon: UsersIcon, current: false },
+  { name: "Reviews", href: "#", icon: FolderIcon, current: false },
+  { name: "Users", href: "#", icon: CalendarIcon, current: false },
+];
+
+export function AdminLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      <div>
+        <Transition.Root show={sidebarOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="fixed inset-0 flex z-40 md:hidden"
+            onClose={setSidebarOpen}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-800 bg-opacity-75" />
+            </Transition.Child>
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-in-out duration-300 transform"
+              enterFrom="-translate-x-full"
+              enterTo="translate-x-0"
+              leave="transition ease-in-out duration-300 transform"
+              leaveFrom="translate-x-0"
+              leaveTo="-translate-x-full"
+            >
+              <div className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-gray-900">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-in-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in-out duration-300"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="absolute top-0 right-0 -mr-12 pt-2">
+                    <button
+                      type="button"
+                      className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                      onClick={() => setSidebarOpen(false)}
+                    >
+                      <span className="sr-only">Close sidebar</span>
+                      <XIcon
+                        className="h-6 w-6 text-white"
+                        aria-hidden="true"
+                      />
+                    </button>
+                  </div>
+                </Transition.Child>
+                <div className="flex-shrink-0 flex items-center px-4">
+                  <Link to="/">
+                    <Logo type="full" />
+                  </Link>
+                </div>
+                <div className="mt-5 flex-1 h-0 overflow-y-auto">
+                  <div className="px-4 mt-4 mb-8">
+                    <Button fullWidth size="sm">
+                      Add New Movie
+                    </Button>
+                  </div>
+                  <nav className="px-4 space-y-2.5">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={clsx(
+                          item.current
+                            ? "bg-gray-800 text-white"
+                            : "text-gray-300 hover:bg-gray-800 hover:text-white",
+                          "group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                        )}
+                      >
+                        <item.icon
+                          className={clsx(
+                            item.current
+                              ? "text-gray-300"
+                              : "text-gray-400 group-hover:text-gray-300",
+                            "mr-4 flex-shrink-0 h-5 w-5"
+                          )}
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </a>
+                    ))}
+                  </nav>
+                </div>
+              </div>
+            </Transition.Child>
+            <div className="flex-shrink-0 w-14" aria-hidden="true">
+              {/* Dummy element to force sidebar to shrink to fit close icon */}
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex-1 flex flex-col min-h-0 border-r border-gray-800">
+            <div className="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
+              <Link to="/">
+                <Logo type="full" />
+              </Link>
+            </div>
+            <div className="flex-1 flex flex-col overflow-y-auto">
+              <div className="px-4 mt-4">
+                <Button fullWidth size="sm">
+                  Add New Movie
+                </Button>
+              </div>
+              <nav className="flex-1 px-4 mt-8 space-y-2.5">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className={clsx(
+                      item.current
+                        ? "bg-gray-800 text-white"
+                        : "text-gray-300 hover:bg-gray-800 hover:text-white",
+                      "group flex items-center px-2.5 py-2 text-sm font-medium rounded-md"
+                    )}
+                  >
+                    <item.icon
+                      className={clsx(
+                        item.current
+                          ? "text-gray-300"
+                          : "text-gray-400 group-hover:text-gray-300",
+                        "mr-3 flex-shrink-0 h-5 w-5"
+                      )}
+                      aria-hidden="true"
+                    />
+                    {item.name}
+                  </a>
+                ))}
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div className="md:pl-64 flex flex-col">
+          <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-gray-900">
+            <button
+              type="button"
+              className="px-4 border-b border-gray-800  text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+              onClick={() => setSidebarOpen(true)}
+            >
+              <span className="sr-only">Open sidebar</span>
+              <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+            <div className="flex-1 px-4 flex justify-end border-b border-gray-800">
+              <div className="ml-4 flex items-center md:ml-6">
+                <ProfileDropdown />
+              </div>
+            </div>
+          </div>
+
+          <main className="flex-1">
+            <div className="py-6">
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                <Outlet />
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
